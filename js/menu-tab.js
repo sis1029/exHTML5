61b05d50ff6f7db1dbfa6864b88026783c5e966a
sunday <ileader7@daum.net>
2019-12-12 14:12:19 +0900

    공지사항및디자인완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -2,6 +2,9 @@ var menu = $('.menu');
 var items = $('.menu-item');
 var btns = $('.btn-menu');
 var anchors = $('.sub-menu a');
+var tabAnchors = $('.board ul a');
+var tabAct = $('.board section');
+var tab = $('.tab');
 
 // 토글 버튼을 클릭했을 때 하위 메뉴를 보이도록 설정할 것
 btns.click(function(e){
@@ -11,3 +14,14 @@ $(this).parent().addClass('menu-act');
 
 });
 anchors.addClass('icon-dot-circled');
+tabAnchors.addClass('icon-dot-circled');
+
+// 탭을 클릭했을 때 클릭한 탭의 목록과 더보기가 보여지도록 설정할 것
+//마우스와 키보드 모두 사용 가능하도록 설정할 것
+tab.on('clic keyup', function(e){
+e.preventDefault();
+if(e.type ==='click' ||(e.type === 'keyup' && e.keyCode === 13)){
+  tabAct.removeClass('tab-act');
+  $(this).parent().addClass('tab-act');
+}
+});
